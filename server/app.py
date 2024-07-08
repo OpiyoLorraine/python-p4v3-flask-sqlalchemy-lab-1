a2ff9653d4af9024bfd27efcb438aee7a4f9086e
OpiyoLorraine <78169855+OpiyoLorraine@users.noreply.github.com>
2024-07-08 11:25:14 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,41 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def get_by_id(id):
+    quake = Earthquake.query.filter_by(id = id).first()
+    if quake:
+        quake_dict = {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+      }
+        status = 200
+    else:
+        quake_dict = {
+            "message": f"Earthquake {id} not found."
+        }
+        status = 404
+    return make_response(quake_dict, status)
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def get_by_magnitude(magnitude):
+    mag = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    mag_dict = {
+        "count": len(mag),
+        "quakes": [
+            {
+                "id": earthquake.id,
+                "location": earthquake.location,
+                "magnitude": earthquake.magnitude,
+                "year": earthquake.year
+            }
+            for earthquake in mag
+        ]
+    }
+    response_code = 200
+    return make_response(mag_dict, response_code)
 
 
 if __name__ == '__main__':
